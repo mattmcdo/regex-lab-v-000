c44b4744c743d580b3b59137df19a3468203dbb1
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-05-31 02:07:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word)
   !!word.match(/\b[aeiouAEIOU]\w+/)
-end
+endb
 
 def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/\w+(ing)/)
 end
 
 def words_five_letters_long(text)
-
+text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
