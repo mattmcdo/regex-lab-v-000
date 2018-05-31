0b476011b52f19129a82bef46391d06d2a00e7a0
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-05-31 01:56:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/\b[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words_string.scan(/\w+(ing)
 end
 
 def words_five_letters_long(text)
