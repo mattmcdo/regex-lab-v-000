6fb22568ab3841698e21378c34bfe89327a895e3
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-05-31 02:41:45 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !!word.match(/\b[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.scan(/^[A-Z].+[,\.]/) != []
 end
 
 def valid_phone_number?(phone)
-
+phone.scan(/(\d( |\W)*){10}\b/) != []
 end
